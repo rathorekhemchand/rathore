cdb7f06f4c74f05d9a7772723a62f5bcfce1bbab
rathorekhemchand <rathorekhemchand311@gmail.com>
2023-04-07 10:46:05 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,48 @@
+var form = prompt("form");
+console.log(form);
+document.write(form);
+document.write("<br/>")
+var physical = prompt("physical");
+console.log(physical);
+document.write(physical);
+document.write("<br/>")
+var fullname = prompt("full name");
+console.log(fullname);
+document.write(fullname);
+document.write("<br/>")
+var fathername = prompt("fathername");
+console.log(fathername);
+document.write(fathername);
+document.write("<br/>")
+var email= prompt("email");
+console.log(email);
+document.write(email);
+document.write("<br/>")
+var password = prompt("password ");
+console.log(password );
+document.write(password );
+document.write("<br/>")
+var cnicnumber = prompt(" cnicnumber");
+console.log( cnicnumber);
+document.write( cnicnumber);
+document.write("<br/>")
+var mobilenumber = prompt("mobilenumber");
+console.log(mobilenumber);
+document.write(mobilenumber);
+document.write("<br/>")
+var selectcourses = prompt("selectcoursesselectcourses");
+console.log(selectcourses);
+document.write(selectcourses);
+document.write("<br/>")
+var selectqualification = prompt("selectqualification");
+console.log(selectqualification);
+document.write(selectqualification);
+document.write("<br/>")
+var selectgender= prompt("selectgender");
+console.log(selectgender);
+document.write(selectgender);
+document.write("<br/>")
+var uploadphoto = prompt("uploadphoto");
+console.log(uploadphoto);
+document.write(uploadphoto);
+document.write("<br/>")
